eb767b18bcf94efc5e03034d19b223858219eef1
jun <leebduk@gmail.com>
2017-12-15 16:06:37 +0900

    add step4

--- step1/launch_servers.sh
+++ step1/launch_servers.sh
@@ -14,7 +14,7 @@ docker run -d \
 --publish 24224:24224/tcp \
 --volume $(pwd)/fluentd:/fluentd \
 --volume ${LOG_FOLDER}:/logs \
-loopchain-fluentd:${TAG}
+loopchain/loopchain-fluentd:${TAG}
 
 
 # RadioStation에서 이용할 데이타 저장 공간을 만듭니다.
@@ -27,7 +27,7 @@ docker run -d --name radio_station \
 -p 7102:7102 \
 -p 9002:9002 \
 --log-driver fluentd --log-opt fluentd-address=localhost:24224 \
-looprs:${TAG} \
+loopchain/looprs:${TAG} \
 python3 radiostation.py -o /conf/rs_conf.json
 
 
@@ -41,5 +41,5 @@ docker run -d --name peer0 \
 --link radio_station:radio_station \
 --log-driver fluentd --log-opt fluentd-address=localhost:24224 \
 -p 7100:7100 -p 9000:9000  \
-looppeer:${TAG} \
+loopchain/looppeer:${TAG} \
 python3 peer.py -o /conf/peer_conf.json  -r radio_station:7102
